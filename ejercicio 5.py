60d83f098150f3d585d7a56b216c819cacacd869
crishzz <mariana.isaza.2004@outlook.com>
2023-02-14 13:21:42 -0500

    Punto 5 con con funciones para el circulo

--- ejercicio 5.py
+++ ejercicio 5.py
@@ -5,3 +5,9 @@ class circle:
     def area(self):
         return 3.1416*(self.radio**2)
     def perimeter(self):
+        return 2*3.1416*self.radio
+    def is_inside(self,point):
+        if (self.center.x-point.x)**2+(self.center.y-point.y)**2<=self.radio**2:
+            return True
+        else:
+            return False
