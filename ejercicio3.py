646b4e9fae13042ee7eb1060e89d980d3b3c509d
crishzz <mariana.isaza.2004@outlook.com>
2023-02-14 13:00:36 -0500

    Ejercicio 3 con nuevas caracteristicas como - Un método mostrar que imprima por consola las coordenadas del punto - Un método mover que cambie las coordenadas del punto - Un método calcular_distancia que calcule la distancia de la instancia

---
+++ ejercicio3.py
@@ -0,0 +1,19 @@
+import ejercicio2
+if __name__ == "__main__":
+    print("Ejercicio 3")
+
+def show_coordinates(self):
+    print("x: ", self.x, "y: ", self.y)
+
+def find_distance(self, other):
+    print ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+
+def move(self,mx,my):
+    self.x += mx
+    self.y += my
+
+
+
+
+
+
