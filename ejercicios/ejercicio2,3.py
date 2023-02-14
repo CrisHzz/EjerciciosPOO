76ecfa85a0109a81eaba2451757b073ebfcd7704
crishzz <mariana.isaza.2004@outlook.com>
2023-02-14 13:43:45 -0500

    Desarollo de los ejercicios 1 al 8

---
+++ ejercicios/ejercicio2,3.py
@@ -0,0 +1,21 @@
+import ejercicio2
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show_coordinates(self):
+     print("x: ", self.x, "y: ", self.y)
+
+    def find_distance(self, other):
+        print ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+
+    def move(self,mx,my):
+        self.x += mx
+        self.y += my
+
+
+
+
+
+
